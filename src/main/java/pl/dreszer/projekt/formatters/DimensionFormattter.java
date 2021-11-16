141d0f4a054d11769563a4f6698948d042d4b4c4
Ja <Jakubiak-123@wp.pl>
2021-11-16 14:40:31 +0100

    Dodano klasę wymiarów, Bootstrap-a, Thymeleaf Layout Dialect, zaczęto tworzyć szablony rozkładów i fragmenty.

--- src/main/java/pl/dreszer/projekt/formatters/DimensionFormattter.java
+++ src/main/java/pl/dreszer/projekt/formatters/DimensionFormattter.java
@@ -1,2 +1,19 @@
-package pl.dreszer.projekt.formatters;public class DimensionFormattter {
+package pl.dreszer.projekt.formatters;
+
+import org.springframework.format.Formatter;
+import pl.dreszer.projekt.models.Dimension;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+public class DimensionFormattter implements Formatter<Dimension> {
+	@Override
+	public Dimension parse(String text, Locale locale) throws ParseException {
+		return null;
+	}
+
+	@Override
+	public String print(Dimension object, Locale locale) {
+		return null;
+	}
 }
--- src/main/java/pl/dreszer/projekt/models/Dimension.java
+++ src/main/java/pl/dreszer/projekt/models/Dimension.java
@@ -1,4 +1,27 @@
 package pl.dreszer.projekt.models;
 
 public class Dimension {
+	private Float width;
+	private Float height;
+
+	public Dimension(Float width, Float heigth) {
+		this.width = width;
+		this.height = heigth;
+	}
+
+	public Float getWidth() {
+		return width;
+	}
+
+	public void setWidth(Float width) {
+		this.width = width;
+	}
+
+	public Float getHeight() {
+		return height;
+	}
+
+	public void setHeight(Float height) {
+		this.height = height;
+	}
 }
